1226f5b4a641e272dff1db38d313e4de3291a81a
Anastasia <n3636@mail.ru>
2024-02-04 19:36:25 +0300

    the first homework result

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
+  let x;
+  let y;
   let arr = [];
-  
+  let D = b ** 2 - 4 * a * c;
+
+  if (D > 0) {
+    x = (-b + Math.sqrt(D))/(2*a)
+    y = (-b - Math.sqrt(D))/(2*a);
+    arr=[x, y]
+  } else if (D == 0) {
+    x = -b/(2*a);
+    arr=[x];
+  } else {
+    arr=[]; 
+  }
+
   return arr;
 }
 
+//процентную ставку(percent), сумму первоначального взноса (contribution), сумму кредита (amount) и срок(countMonths)
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let S = amount - contribution;
+  let P = percent / 100 / 12;
+  let creditMonthly = S * (P + (P / (((1 + P)**countMonths) - 1)));
+  let credit = parseFloat((creditMonthly * countMonths).toFixed(2));
+
+  return credit;  
 }
