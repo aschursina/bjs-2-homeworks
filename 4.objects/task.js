a58f0e6b89aea6c2fb74cd2b115e9c449d60767d
Anastasia <n3636@mail.ru>
2024-02-16 22:50:17 +0300

    edit task object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,8 +1,8 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 let student = new Student("Василиса", "женский", 19);
@@ -12,31 +12,26 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...addMark) {
-  if (student.hasOwnProperty('marks') == true) {
+  if (this.hasOwnProperty('marks')) {
       this.marks.push(...addMark);
-      return addMark;
-   } else { 
-    return 0;
-   }
+   } 
 }
 
  Student.prototype.getAverage = function () {
-  if (student.hasOwnProperty('marks') == false || student.marks == []) {
+  if (this.hasOwnProperty('marks') === false || this.marks.length < 1) {
     return 0;
-  } else {
-    let result = this.marks.reduce((acc, item, index, arr) => {
-        acc+=item;
-        if (index === arr.length - 1) {
+  } 
+  return result = this.marks.reduce((acc, item, index, arr) => {
+    acc+=item;
+    if (index === arr.length - 1) {
         return acc / arr.length;
-        }
-        return acc;
-    }, 0) ;
-    return result;
-  }
+       }
+       return acc;
+  }, 0) ;
 } 
 
 Student.prototype.exclude = function (reason) {
-    delete this.marks;
-    delete this.subject;
+  delete this.marks;
+  delete this.subject;
     this.excluded = reason;
 }
